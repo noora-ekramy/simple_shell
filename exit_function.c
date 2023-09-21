f0becd5fc3a2eee20f37e95a64f18c1730f21224
Nora <nora.ekramy11@gmail.com>
2023-09-21 15:45:43 -0700

    exit string

--- exit_function.c
+++ exit_function.c
@@ -1,4 +1,27 @@
 #include "shell.h"
+
+int is_int(char *input)
+{
+	int i=0;
+
+	while(input[i] != '\0')
+	{
+		if(input[i] <= '0' || input[i] >= '9' )
+		{
+			if(input[i] == '-' || input[i] =='-')
+			{
+				i++;
+				continue;
+			}
+			return 0;
+		}
+		i++;
+	}
+	return 1;
+	
+}
+
+
 /**
  * exit_function - perform exit
  * @args: arguments
@@ -14,6 +37,11 @@ int exit_function(char *args[] , int last)
 		argCount++;
 	if (argCount > 1)
 	{
+		if(is_int(args[1])== 0)
+		{
+			 fprintf(stderr, "./hsh: 1: exit: Illegal number: %d\n", exit_code);
+                        return(2);
+		}
 		exit_code = _atoi(args[1]);
 		free(args);
 		if(exit_code<0)
